e7db9ae87278064c13c207acdbb7f5475534d7bc
AnB School 0002 <anbschool0002@AnBs-Mac-mini-107.local>
2024-08-22 14:59:56 +0700

    Navigate automatically

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 export const Home = () => {
+  const navigate = useNavigate();
+
+  const handleNavigate = () => {
+    setTimeout(() => {
+      console.log('Navigate')
+      navigate('/product');
+    }, 2000);
+  };
+
   return (
     <div>
       <h1>Home</h1>
       <Link to="/product">Go to Product</Link>
+      <p>Click The button to navigate automatically</p>
+      <button onClick={handleNavigate}>Click Me Delay 2 Second</button>
     </div>
   );
 };
